6c81ea820beb416e6908170a169235c58eca55b8
Albaraa Taher Zeinoglu <65316209+Albaraazain@users.noreply.github.com>
2025-01-04 03:58:36 +0300

    Update flutter_export_environment.sh for Windows compatibility
    - Changed FLUTTER_ROOT and FLUTTER_APPLICATION_PATH to Windows file paths to support development on a Windows environment.
    - Updated FLUTTER_TARGET to use backslashes for path consistency.
    
    These changes ensure the Flutter environment is correctly configured for Windows users, enhancing cross-platform development capabilities.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/albaraamac/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/albaraamac/Documents/projects/autovista"
+export "FLUTTER_ROOT=C:\src\flutter_windows_3.24.3-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Pc\StudioProjects\autovista_version_2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
